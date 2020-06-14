8033a63a3bf8aa664c29045789c7c3cc0adf5ec7
Neville <live.neville@live.com>
2020-06-14 03:29:51 -0700

    added icon

--- src/MonthlySubscriptions/MonthlySubscriptions.Android/MainActivity.cs
+++ src/MonthlySubscriptions/MonthlySubscriptions.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MonthlySubscriptions.Droid
 {
-    [Activity(Label = "MonthlySubscriptions", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MonthlySubscriptions", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
